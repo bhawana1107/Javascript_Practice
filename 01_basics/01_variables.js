c8a1312f3c774987d243b10571bfb076abc817fc
Bhawana <103374144+bhawana1107@users.noreply.github.com>
2025-02-26 18:38:45 +0000

    Variable Practice

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553 
+let accountEmail = "bhawana@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+// accountId = 2 // not allowed
+
+accountEmail = "bhanu@gmail.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+console.log(accountId );
+
+/*
+*{
+}
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+
