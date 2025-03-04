e022faf66ea517d130017a3c5c094575bc35b0a5
Bhawana <103374144+bhawana1107@users.noreply.github.com>
2025-03-04 18:59:13 +0000

    Dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,9 @@
+// dates
+
+const myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
