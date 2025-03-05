ebf66ab5d26dce8f7e2eca097b27572fa6de8347
Bhawana <103374144+bhawana1107@users.noreply.github.com>
2025-03-05 18:03:21 +0000

    Dates

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,9 +1,30 @@
 // dates
 
-const myDate = new Date();
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toDateString());
-console.log(typeof myDate);
+// const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
 
+// let myCreatedDate = new Date(2023 , 0 , 23)
+// let myCreatedDate = new Date(2023 , 0 , 23 , 5, 3)
+//  let myCreatedDate = new Date("2023-01-14");
+ let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate.getMonth() +  1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday: "long" ,
+})
