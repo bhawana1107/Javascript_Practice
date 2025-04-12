34e5b25ac876bd598fdcd77f30e70fc860f144e1
Bhawana <103374144+bhawana1107@users.noreply.github.com>
2025-04-12 19:21:17 +0000

    change

--- step_progress_bar/script.js
+++ step_progress_bar/script.js
@@ -1,7 +1,32 @@
+// function handleClick(el) {
+//    let steps = document.querySelectorAll('.step');
+
+//     for (let i= 1; i < steps.length; i++){
+//         if (!(steps[i].classList.contains('checked'))) {
+//             steps[i].classList.add('checked');
+//             break; 
+        
+//         }
+//         else{
+//            console.log("okk");
+            
+//         }
+//     }
+//    console.log(el.id);
+//    console.log(steps);
+// }
+
 function handleClick(el) {
-    let step = [0 , 1 , 2 , 3 , 4 ,5];
-    console.log(step.classList.add());
-    
-    console.log(el.id);
-    
+    let steps = document.querySelectorAll('.step');
+
+    for (let i = 0; i < steps.length; i++) {
+        // Find the first step that is NOT checked
+        if (!steps[i].classList.contains('checked')) {
+            steps[i].classList.add('checked');
+            console.log("Checked added to:", steps[i].textContent);
+            break;
+        }
+    }
+
+    console.log("Button ID:", el.id);
 }
